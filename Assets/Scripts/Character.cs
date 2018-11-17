9fff32440b72683039aba62cdec939fd6d895555
Teo-CD <teo.couprie_diaz@telecom-sudparis.eu>
2018-11-17 12:18:15 +0100

    Début de l'UI Ajout d'Ink/INkle Ajout de la classe de character

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	[SerializeField]
+	private Age age;
+	private Age acceptsAge;
+
+	private int dialogueProgression;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
