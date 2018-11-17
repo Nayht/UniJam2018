f66134ff9cacfe0844154775bf6807ce63bd93ad
Teo-CD <teo.couprie_diaz@telecom-sudparis.eu>
2018-11-17 16:07:30 +0100

    Résolution du problème d'import d'asset textuel après ***beaucoup trop de temps***

--- Assets/Scripts/DialogueEngine.cs
+++ Assets/Scripts/DialogueEngine.cs
@@ -8,14 +8,57 @@ public class DialogueEngine : MonoBehaviour
 
 	private Story story;
 	
+	[SerializeField]
+	private string storyLocation;
+
+	[SerializeField]
+	private TextAsset storyAsset;
+	
 	// Use this for initialization
 	void Start ()
 	{
-		story = new Story("Assets/Dialogues/New Ink.json");
+		if (string.IsNullOrEmpty(storyLocation))
+		{
+			storyLocation = "Dialogues/New_Ink";
+		}
+
+		storyAsset = Resources.Load(storyLocation) as TextAsset;
+		story = new Story(storyAsset.text);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		Progress();
+	}
+
+	void UpdateGUI(string dialogueOutput)
+	{
 		
+	}
+	
+	void Progress()
+	{
+		if (story.canContinue)
+		{
+			Debug.Log(story.Continue());
+		}
+		else
+		{
+			if (story.currentChoices.Count > 0)
+			{
+				manageChoices();
+			}
+		}
+	}
+
+	void manageChoices()
+	{
+		for (int i = 0; i < story.currentChoices.Count; i++)
+		{
+			Choice choice = story.currentChoices[i];
+			Debug.Log("Choix : "+choice.text);
+		}
+		story.ChooseChoiceIndex(0);
 	}
 }
