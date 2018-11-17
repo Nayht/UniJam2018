53ad3259f60265ce280b140f5aeeddce521ff52c
Teo-CD <teo.couprie_diaz@telecom-sudparis.eu>
2018-11-17 14:43:24 +0100

    Début de l'arbre de l'intégration de Ink dans une DialogueEngine

---
+++ Assets/Dialogues/New Ink.ink
@@ -0,0 +1,15 @@
+
+-> Start
+=== Start
+
+Salutations étranger !
+Banana man
+-> part2
+
+= part2
+
+THE CHOICE BEGINS
+* Choose me ! []
+    I'll bring you happiness -> END
+* Kill me []
+    I'll bring you despair   -> END
---
+++ Assets/Scripts/DialogueEngine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+
+public class DialogueEngine : MonoBehaviour
+{
+
+	private Story story;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		story = new Story("Assets/Dialogues/New Ink.json");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
