dd7a91296fd2e6a1b4a55fc6bf90870df543abb0
MasterEwink <mad@minet.net>
2018-11-17 02:28:01 +0100

    add unity project

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	// over MonoBehaviours
+	private MoveEngine moveEngine;
+	private Transform transform;
+
+	// to keep track of the mouse position to chase it
+	private bool chase_mouse;
+	private Vector2 mouse_position;
+	[SerializeField]
+	private float distance_stop = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		moveEngine = GetComponent<MoveEngine>();
+		transform = GetComponent<Transform>();
+		chase_mouse = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movementController();
+	}
+
+	// gets the input and generates a direction for moveEngine.move
+	void movementController() {
+		Vector2 direction = Vector2.zero; // the direction of the movement
+		Vector2 keyboard_direction; // the direction given by the keyboard
+		Vector2 position = new Vector2(transform.position.x, transform.position.y);
+		// MOUSE
+		if (Input.GetMouseButtonDown(0)) // left click
+		{
+			// TODO : Need to check if anyone is under the mouse, you don't want to keep walking into people eternally
+			chase_mouse = chase_mouse == false;
+			mouse_position = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+		}
+		if (chase_mouse)
+		{
+			direction = mouse_position - position;
+			// if we are close enough we stop
+			if (direction.magnitude < distance_stop)
+			{
+				chase_mouse = false;
+			}
+		}
+		// KEYBOARD
+		keyboard_direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		// the keyboard is prioritary
+		if (keyboard_direction.magnitude > 0)
+		{
+			direction = keyboard_direction;
+			chase_mouse = false;
+		}
+		// MOVE
+		moveEngine.move(direction);
+	}
+}
---
+++ Assets/Scripts/MoveEngine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEngine : MonoBehaviour {
+
+	Rigidbody2D rb;
+	[SerializeField]
+	private float speed_old = 0.8f;
+	[SerializeField]
+	private float speed_adult = 1.0f;
+	[SerializeField]
+	private float speed_teen = 1.5f;
+	[SerializeField]
+	private float speed_child = 1.2f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	public void move(Vector2 direction) {
+		Vector2 velocity = direction;
+		velocity.Normalize();
+		// normalize depending on age
+		if (true) {
+			velocity = velocity*speed_adult;
+		}
+		rb.velocity = velocity;
+	}
+}
